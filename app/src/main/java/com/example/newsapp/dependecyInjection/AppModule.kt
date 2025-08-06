0c28b023cff7ab33539930208f8626984d65ea09
Shreelakshmi-Hegde <shreelakshmi.hegde@syntacticlogic.com>
2025-08-06 21:27:32 +0530

    #7 News Data, Api

--- app/src/main/java/com/example/newsapp/dependecyInjection/AppModule.kt
+++ app/src/main/java/com/example/newsapp/dependecyInjection/AppModule.kt
@@ -2,14 +2,23 @@ package com.example.newsapp.dependecyInjection
 
 import android.app.Application
 import com.example.newsapp.data.manager.LocalUserImplementation
+import com.example.newsapp.data.remote.dataTranferObject.NewsApi
+import com.example.newsapp.data.repository.NewsRepositoryImp
 import com.example.newsapp.domain.manager.LocalUserManager
+import com.example.newsapp.domain.repository.NewsRepository
 import com.example.newsapp.domain.usecases.app_entry.AppEntryUsecases
 import com.example.newsapp.domain.usecases.app_entry.ReadAppEntry
 import com.example.newsapp.domain.usecases.app_entry.SaveAppEntry
+import com.example.newsapp.domain.usecases.news.GetNews
+import com.example.newsapp.domain.usecases.news.NewsUseCases
+import com.example.newsapp.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 
@@ -33,4 +42,28 @@ object AppModule {
         readAppEntry = ReadAppEntry(localUserManager),
         saveAppEntry = SaveAppEntry(localUserManager)
     )
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(): NewsApi{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(
+        newsApi: NewsApi
+    ): NewsRepository= NewsRepositoryImp(newsApi)
+
+    @Provides
+    @Singleton
+    fun provideNewsUseCases(newsRepository: NewsRepository): NewsUseCases{
+        return NewsUseCases(
+            getNews = GetNews(newsRepository)
+        )
+    }
 }
--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -6,4 +6,6 @@ object Constants {
     const val APP_ENTRYS="appEntry"
 
     const val API_KEY="57db6117962143f7ae3634a3b5a766ae"
+
+    const val BASE_URL="https://newsapi.org/v2/"
 }
